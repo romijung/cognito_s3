edcfb583d073af4c629781126366656b420d3ab7
romijung <romi.jung@samsung.com>
2020-09-09 13:10:10 +0900

    Update s3.js

--- src/config/s3.js
+++ src/config/s3.js
@@ -1,6 +1,6 @@
 const obj = {
-  BUCKET_NAME: '',
-  AWS_REGION: '',
+  BUCKET_NAME: 'jromi-test-webhost',
+  AWS_REGION: 'us-east-1',
   COGNITO_IDENTITY_POOL_ID: '',
   COGNITO_USER_POOL_ID: '',
   COGNITO_CLIENT_ID: ''
